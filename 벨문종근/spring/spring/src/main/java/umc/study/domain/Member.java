5371262fee566bd6a92d85b9eeb4269ba2caa200
jgmoon <6813821@tukorea.ac.kr>
2023-11-11 22:22:01 +0900

    [ADD] Member 엔티티 추가

---
+++ 벨문종근/spring/spring/src/main/java/umc/study/domain/Member.java
@@ -0,0 +1,44 @@
+package umc.study.domain;
+
+import lombok.*;
+import umc.study.domain.enums.Gender;
+import umc.study.domain.enums.MemberStatus;
+import umc.study.domain.enums.SocialType;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    // 어떤 값이 저장될지 모르는 경우가 아니라 정해진 값들 중에 특정한 값이 저장
+    // enum 타입 사용
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    private String specAddress;
+
+    private LocalDate inactiveDate;
+
+    private Long email;
+
+    private int point;
+}
